d0edf9c349d7f93190f7e6635595f09bcf5c093b
Sumaya <sumayayasin25@gmail.com>
2025-12-14 18:02:12 +0000

    Initial commit: Add ClarityScan app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>ClarityScan</h1>
+     <p>Track your skin. See changes. Gain insights</p>
+    </div>
+  );
+}
+
+export default App;
