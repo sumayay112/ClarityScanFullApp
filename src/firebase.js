0427599841bacd4a1a248383a697debd03d39538
Sumaya Yasin <sumayayasin@Sumayas-MacBook-Air.local>
2026-04-30 05:02:24 +0100

    updated firebase config to use env variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,21 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage"; // 
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCdsCDkYwrhqJT7dd1vUgufwjMBgJ9fAgo",
-  authDomain: "clarityscan-a6ccf.firebaseapp.com",
-  projectId: "clarityscan-a6ccf",
-  storageBucket: "clarityscan-a6ccf.firebasestorage.app",
-  messagingSenderId: "916331343818",
-  appId: "1:916331343818:web:cc10452b9e3face6b8a933",
-  measurementId: "G-R4EZ3QE75R"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-export const storage = getStorage(app); e
+export const storage = getStorage(app);
